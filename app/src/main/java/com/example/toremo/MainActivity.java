7b7775ed41e7d8cf23c2b159348f73fde03bc840
yshibasaki <shibasaki.yutaro@gb-sense.com>
2020-02-28 13:28:57 +0900

    Initial commit

---
+++ app/src/main/java/com/example/toremo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.toremo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText ninzuu; //人数への参照
+    private EditText goukei; //合計金額への参照
+    private TextView kekka; //結果への参照
+    private Button start; //スタートボタンへの参照
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
